64093d220616762e5222c4542755ceedd6f25d90
Thanhkieutt <47922030+Thanhkieutt@users.noreply.github.com>
2020-01-01 21:35:24 +0700

    Add files via upload

---
+++ home.php
@@ -0,0 +1,54 @@
+<?php include('includes/header.php') ?>
+
+    <div id="content-wrapper">
+        <div class="container-fluid">
+            <!-- DataTables Example -->
+            <div class="card mb-3">
+                <div class="card-header">
+                    <i class="fas fa-table"></i>
+                    Thông Tin
+                </div>
+                <div class="card-body"></div>
+                <div class="Home" style="padding:20px 30px; background-color:beige;border-radius:20px;margin:20px 20px;text-align:justify">
+    <h2>Giới Thiệu</h2>
+
+    Thư viện là trung tâm thông tin, trung tâm văn hóa, khoa học của trường ĐH Khánh Hòa .
+    Thư viện có chức năng cung cấp tri thức và thông tin – tư liệu về các lĩnh vực KHXH & NV trong và ngoài nước,
+    phục vụ hoạt động đào tạo, nghiên cứu khoa học của trường. Thư viện có trách nhiệm tổ chức, quản lý, bổ sung,
+    thu thập, bảo quản các tài liệu, sách, báo, tạp chí, băng, đĩa, các luận án đã bảo vệ tại trường, các ấn phẩm
+    của trường và các tài liệu lưu trữ khác, hướng dẫn và quản lý công tác quyền sở hữu trí tuệ của trường.
+
+    <h2>Mục tiêu chung</h2>
+
+    Xây dựng thư viện đạt chuẩn của một thư viện hiện đại với nguồn tài nguyên thông tin phong phú,
+    đa dạng, đáp ứng đầy đủ nhu cầu đào tạo và nghiên cứu khoa học về các lĩnh vực KHXH&NV, hướng tới đáp
+    ứng nhu cầu của người sử dụng thông tin KHXH&NV trong các trường đại học Việt Nam.
+
+    <h2>Cơ Sở Vật Chất</h2>
+
+    Trung tâm Học liệu - Thư viện được xây dựng với qui mô 4 tầng (3.420m2 xây dựng,
+    diện tích sàn: 4.200m2; sân đường nội bộ, vườn hoa), được trang bị các trang thiết bị,
+    kỹ thuật hiện đại, đồng bộ, tự động hóa. Các tầng được bố trí như sau: Tầng 1 bao gồm tiền sảnh,
+    sảnh chính, hội trường, kho giáo trình và phòng hành chính; Tầng 2 gồm phòng đọc lớn, phòng tra
+    cứu tài liệu mật, kho tài liệu mật; Tầng 3 gồm phòng đọc lớn, phòng tra cứu tài liệu và kho lưu trữ;
+    Tầng 4 gồm phòng đọc sau đại học, phòng đa phương tiện, 4 phòng hội thảo, phòng học nhóm, kho và khu vực sân vườn.
+    Việc xây dựng và đưa Trung tâm Học liệu - Thư viện vào hoạt động để đáp ứng nhu cầu nghiên cứu, giảng dạy và học tập
+    của cán bộ, giảng viên, học viên, sinh viên, đánh dấu một bước phát triển quan trọng trong công tác giáo dục - đào 
+    tạo của Nhà trường. Trung tâm Học liệu - Thư viện sẽ thực sự là nơi cung cấp nguồn thông tin quan trọng; là điểm kết
+    nối giữa nguồn thông tin của xã hội và nhu cầu thông tin của cán bộ, giảng viên, học viên, sinh viên; là môi trường 
+    rèn luyện và phát huy năng lực độc lập trong việc khám phá và tư suy sáng tạo của học viên, sinh viên và nó còn góp
+    phần cải tiến nội dung chương trình, phương pháp giảng dạy. Việc xây dựng và đưa Trung tâm Học liệu - Thư viện vào 
+    hoạt động còn tạo tiền đề quan trọng xây dựng Nhà trường theo hướng chuẩn hóa, hiện đại hóa, trở thành một cơ sở 
+    giáo dục - đào tạo, nghiên cứu khoa học uy tín trong hệ thống các ngành và hệ thống giáo dục đại học quốc gia, đáp 
+    ứng yêu cầu nhiệm vụ của giáo viên, sinh viên nhà trường, nhu cầu phát triển kinh tế - xã hội, công nghiệp hóa - 
+    hiện đại hóa đất nước và hội nhập quốc tế...
+    <br />
+    <img class="anh" src="Content/a3.jpg" style="max-width:400px;max-height:300px;margin-left:50px;" />
+    <img class="anh" src="Content/a4.jpg" style="max-width:400px;max-height:300px;margin-left:50px;" />
+                </div>
+            </div>
+        </div>
+        <!-- /.container-fluid -->
+    </div>
+</div>
+<?php include('includes/footer.php')?>
